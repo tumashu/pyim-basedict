06a191ce95cb81459839d7a32becceea50a79384
Feng Shu <tumashu@163.com>
2021-03-11 08:58:17 +0800

    Update pyim-basedict.el header.

--- pyim-basedict.el
+++ pyim-basedict.el
@@ -1,31 +1,27 @@
 ;;; pyim-basedict.el --- The default pinyin dict of pyim  -*- lexical-binding: t; -*-
 
 ;; * Header
-;; Copyright (C) 2015 Feng Shu <tumashu@163.com>
+;; Copyright (C) 2015-2021 Free Software Foundation, Inc.
 
 ;; Author: Feng Shu <tumashu@163.com>
 ;; URL: https://github.com/tumashu/pyim-basedict
 ;; Version: 0.4.0
 ;; Keywords: convenience, Chinese, pinyin, input-method, complete
 
-;;; License:
+;; This file is part of GNU Emacs.
 
-;; This file is not part of GNU Emacs.
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 3
-;; of the License, or (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; * pyim-basedict README                         :README:doc:
